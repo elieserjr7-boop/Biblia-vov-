098cf4f078c8ee99f68f13e5df9232e4eaecaed8
elieserjr7-boop <elieserjr7@ufpi.edu.br>
2026-01-21 00:21:04 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // --- IMPORTANTE: Mude este número a cada Deploy (v14, v15, v16...) ---
-const CACHE_NAME = 'biblia-sagrada-v32';
+const CACHE_NAME = 'biblia-sagrada-v33';
 
 // Lista de arquivos
 const urlsToCache = [
